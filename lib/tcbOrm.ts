cf425cc244616e22cec7e77121d1fc53867d1d12
ForChange客服小弟 <137819866@qq.com>
2022-03-25 16:57:39 +0800

    type: 增强类型
    建议开启
    ```
    // tsconfig.ts
        "noUncheckedIndexedAccess": true,
        "strictNullChecks": true
    ```

--- lib/tcbOrm.ts
+++ lib/tcbOrm.ts
@@ -1,238 +1,51 @@
-import { Init, Inject } from '@midwayjs/decorator';
-import { CloudbaseService } from 'midway-cloudbase';
-import { Filter, UpdateFilter } from 'mongodb';
-import { Database, Database as TcbDatabase } from '@cloudbase/node-sdk/types';
+import { Filter } from 'mongodb';
+import { ISearchReq } from '../interface';
 
-type WithOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+const transformSort = (sort?: string) => {
+  if (!sort) return {};
+  const [fieldPath, directionStr] = sort.split(':');
+  if (!fieldPath || !directionStr) return {};
+  return {
+    orderBy: { [fieldPath]: directionStr.toLowerCase() as 'desc' | 'asc' },
+  };
+};
 
-export interface IRecordBase {
-  _id: string;
-  createTime: number;
-  updateTime: number;
-}
+const transformFilters = (filters?: Filter<{ [prop: string]: any }>) => {
+  if (!filters?.$and?.length) return {};
 
-export interface IRecordType extends IRecordBase {
-  [prop: string]: any;
-}
-
-export default class TcbOrm<IRecord extends IRecordType> {
-  @Inject()
-  tcbService: CloudbaseService;
-
-  tableName: string;
-  db: Database.Db;
-  coll: Database.CollectionReference;
-
-  @Init()
-  async init() {
-    this.db = this.tcbService.database();
-    this.coll = this.db.collection(this.tableName);
-  }
-
-  async createMany(
-    data: Array<
-      Omit<WithOptional<IRecord, 'updateTime' | 'createTime' | '_id'>, '_id'>
-    >
-  ) {
-    const res = await this.coll.add(
-      data.map(item => ({
-        updateTime: +new Date(),
-        createTime: +new Date(),
-        ...item,
-      }))
-    );
-
-    return res;
-  }
-
-  async create(
-    data: Omit<
-      WithOptional<IRecord, 'updateTime' | 'createTime' | '_id'>,
-      '_id'
-    >
-  ) {
-    const res = await this.coll.add({
-      updateTime: +new Date(),
-      createTime: +new Date(),
-      ...data,
-    });
-
-    return res;
-  }
-
-  async update({
-    where,
-    data,
-  }: {
-    where: Filter<IRecord>;
-    data: UpdateFilter<IRecord>;
-  }) {
-    const res = await this.coll
-      .where(where)
-      .limit(1)
-      .updateAndReturn({ updateTime: +new Date(), ...data });
-
-    return res.doc as IRecord;
-  }
-
-  async upsert({
-    where,
-    update,
-    create,
-  }: {
-    where: Filter<IRecord>;
-    update: UpdateFilter<IRecord>;
-    create: Omit<
-      WithOptional<IRecord, 'updateTime' | 'createTime' | '_id'>,
-      '_id'
-    >;
-  }) {
-    const record = await this.findUnique({ where });
-    if (record) {
-      return this.update({ where, data: update });
-    } else {
-      return this.create(create);
-    }
-  }
-
-  async updateMany({
-    where,
-    data,
-  }: {
-    where: Filter<IRecord>;
-    data: UpdateFilter<IRecord>;
-  }) {
-    await this.coll.where(where).update({ updateTime: +new Date(), ...data });
-  }
-
-  async updateById(id: string, data: UpdateFilter<IRecord>) {
-    return this.update({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore type
-      where: {
-        _id: { $eq: id },
-      },
-      data,
-    });
-  }
-
-  async query(args: {
-    where?: Filter<IRecord>;
-    skip?: number;
-    take?: number;
-    orderBy?: Partial<Record<keyof IRecord, Database.OrderByDirection>>;
-  }) {
-    const newArgs = {
-      ...args,
-      skip: args.skip ?? 0,
-      take: args.take ?? 10,
-    };
-
-    const [data, { total }] = await Promise.all([
-      this.findMany(newArgs),
-      this.count(newArgs),
-    ]);
-
-    return {
-      list: data,
-      total,
-      pageSize: newArgs.take,
-      page: newArgs.skip * newArgs.take,
-    };
-  }
-
-  async findMany({
-    where,
-    skip = 0,
-    take = 10,
-    orderBy,
-    select,
-  }: {
-    where?: Filter<IRecord>;
-    skip?: number;
-    take?: number;
-    orderBy?: Partial<Record<keyof IRecord, Database.OrderByDirection>>;
-    select?: Partial<Record<keyof IRecord, boolean>>;
-  }) {
-    let query = this.coll.where(where || {});
-
-    if (orderBy) {
-      const orderByKeys = Object.entries(orderBy);
-      if (orderByKeys.length) {
-        const [key, direction] = orderByKeys[0];
-        query = query.orderBy(key, direction);
+  const handler = (obj: Filter<{ [prop: string]: any }>) => {
+    const object = { ...obj };
+    for (const key in object) {
+      if (Object.prototype.hasOwnProperty.call(object, key)) {
+        if (key === '$not') {
+          object[key] = new RegExp(String(obj[key]));
+        }
+        if (
+          typeof object[key] === 'object' &&
+          Object.keys(object[key]).length
+        ) {
+          object[key] = handler(object[key]);
+        }
       }
     }
-
-    if (select) {
-      query = query.field(select);
-    }
-
-    const { data } = await query.skip(skip).limit(take).get();
-
-    return data as IRecord[];
-  }
-
-  async delete({ where }: { where?: Filter<IRecord> }) {
-    const record = await this.findUnique({ where });
-    const res = await this.coll.doc(record._id).remove();
-    return res;
-  }
-
-  async deleteMany({ where }: { where?: Filter<IRecord> }) {
-    const res = await this.coll.where(where).remove();
-    return res;
-  }
-
-  async findUnique({
-    where,
-    orderBy,
-  }: {
-    where?: Filter<IRecord>;
-    orderBy?: Partial<Record<keyof IRecord, Database.OrderByDirection>>;
-  }) {
-    const res = await this.findMany({
-      where: where,
-      take: 1,
-      orderBy,
-    });
-
-    return res[0] ? res[0] : null;
-  }
-
-  async findById(id: string) {
-    const res = await this.findUnique({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore type
-      where: {
-        _id: { $eq: id },
-      },
-    });
-    return res ? res : null;
-  }
-
-  async count({ where }: { where?: Filter<IRecord> }) {
-    const query = this.coll.where(where || {});
-    const { total } = await query.count();
-
-    return {
-      total,
-    };
-  }
-
-  aggregate() {
-    const aggregate = this.coll.aggregate() as TcbDatabase.Aggregation;
-
-    return aggregate;
-  }
-
-  startTransaction() {
-    return this.db.startTransaction();
-  }
-
-  withTransaction(transaction: Database.Transaction) {
-    return Object.assign(Object.create(Object.getPrototypeOf(this)), this, {
-      coll: transaction.collection(this.tableName),
-    }) as this;
-  }
+    return object;
+  };
+  return {
+    where: {
+      $and: filters.$and.map(handler),
+    },
+  };
+};
+export function transformSearchReqToQuery({
+  page,
+  pageSize,
+  sort,
+  filters,
+}: ISearchReq) {
+  return {
+    take: pageSize ? pageSize : undefined,
+    skip: page && pageSize ? page * pageSize : undefined,
+    ...transformSort(sort),
+    ...transformFilters(filters),
+  };
 }
